2f5ab30562e1a79265827333dd29c69d3e4f2eec
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-12 20:39:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,9 @@
 def map(source_array)
-  neg = []
-  ori = []
-  two = []
-  exp = []
+  new = []
   i = 0
   while i < source_array.length do
-    neg.push(source_array[i]*-1)
-    ori.push(source_array[i])
-    two.push(source_array[i]*2)
-    exp.push(source_array[i]**2)
+    neg.push(yield(source_array[i]))
     i += 1
   end
-  return neg, ori, two, exp
+  return new
 end
