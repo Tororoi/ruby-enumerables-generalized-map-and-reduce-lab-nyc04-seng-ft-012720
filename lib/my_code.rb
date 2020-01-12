2329e5be08e9545ac39eef9b1ddb51638463d029
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-12 20:46:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,17 +8,17 @@ def map(source_array)
   new
 end
 
-def reduce(source_array, sp=nil)
-    if sp
-      accum = sp
-      i = 0
-    else
-      accum = s[0]
-      i = 1
-    end
-    while i < source_array.length
-      accum = yield(accum, source_array[i])
-      i += 1
-    end
-    accum
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
 end
