391925ccc18b33c522df2cf60310f1f2346ff692
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-12 20:29:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,14 @@
 def map(source_array)
-  mod = -1
+  mod = [-1,1,2,*2]
+  imod = 0
   new = []
   i = 0
+while imod < mod.length do
   while i < source_array.length do
-    new.push(source_array[i]*mod)
+    new.push(source_array[i]*mod[imod])
     i += 1
   end
+  imod +=1
+end
   return new
 end
